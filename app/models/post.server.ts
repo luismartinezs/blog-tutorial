0542a7096e1170eef843e2261004ecc0b45b82cb
Luis Martinez <luismartinezwebdev@gmail.com>
2023-07-11 14:41:25 +0200

    chore: end of tut

---
+++ app/models/post.server.ts
@@ -0,0 +1,35 @@
+import type { Post } from "@prisma/client";
+
+import { prisma } from "~/db.server";
+
+export async function getPosts() {
+  return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({
+    where: { slug },
+  })
+}
+
+export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+  return prisma.post.create({ data: post })
+}
+
+// type Post = {
+//   slug: string
+//   title: string
+// }
+
+// export async function getPosts(): Promise<Post[]> {
+//   return [
+//     {
+//       slug: "my-first-post",
+//       title: "My First Post",
+//     },
+//     {
+//       slug: "90s-mixtape",
+//       title: "A Mixtape I Made Just For You",
+//     },
+//   ]
+// }
